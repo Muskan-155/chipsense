9e03d3c97095e1bc868dc0266f125ffef0f42423
Muskan <your-email@example.com>
2026-07-06 14:03:12 +0530

    Day 3: Backend deployment updates

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const authRoutes = require("./routes/auth.routes");
+const connectDB = require("./config/db");
 
 dotenv.config();
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("ChipSense Backend Running...");
